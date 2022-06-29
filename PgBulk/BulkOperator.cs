9a7953ff15271c2a368741357257a8794c64065a
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2022-06-29 17:10:43 -0300

    Moves ManualBulkOperator to its own file

--- PgBulk/BulkOperator.cs
+++ PgBulk/BulkOperator.cs
@@ -1,47 +1,9 @@
-﻿using System.Text;
+using System.Diagnostics;
+using System.Text;
 using Npgsql;
 using PgBulk.Abstractions;
 
 namespace PgBulk;
-
-public sealed class ManualBulkOperator : BulkOperator
-{
-    public ManualBulkOperator(string? connectionString, ManualTableInformationProvider tableInformationProvider) : base(connectionString, tableInformationProvider)
-    {
-    }
-
-    public async Task VerifyPrimaryKeys()
-    {
-        if (TableInformationProvider is not ManualTableInformationProvider manualTableInformationProvider)
-        {
-            throw new InvalidOperationException("Table information provider is not of manual type");
-        }
-
-        foreach (var manualTableInformation in manualTableInformationProvider.TableColumnInformations.Values)
-        {
-            await using var connection = await CreateOpenedConnection();
-            var command = connection.CreateCommand();
-            var scriptBuilder = new StringBuilder();
-            scriptBuilder.AppendLine("SELECT a.attname");
-            scriptBuilder.AppendLine("FROM pg_index i");
-            scriptBuilder.AppendLine("JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)");
-            scriptBuilder.AppendLine($"WHERE  i.indrelid = '\"{manualTableInformation.Name}\"'::regclass");
-            command.CommandText = scriptBuilder.ToString();
-            var reader = await command.ExecuteReaderAsync();
-            var primaryKeys = new HashSet<string>();
-
-            while (await reader.ReadAsync())
-            {
-                primaryKeys.Add(reader.GetString(0));
-            }
-
-            foreach (var tableColumnInformation in manualTableInformation.Columns.OfType<ManualTableColumnMapping>())
-            {
-                tableColumnInformation.PrimaryKey = primaryKeys.Contains(tableColumnInformation.Name);
-            }
-        }
-    }
-}
 
 public class BulkOperator
 {

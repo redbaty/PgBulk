c8a36a585679d20e5e30b7fbc10f1fc3c78c0177
Marcos Cordeiro <mvmcjunior@gmail.com>
2023-10-02 15:55:14 -0300

    Pass useContextConnection to extension methods

--- PgBulk.EFCore/ContextExtensions.cs
+++ PgBulk.EFCore/ContextExtensions.cs
@@ -4,21 +4,21 @@ namespace PgBulk.EFCore;
 
 public static class ContextExtensions
 {
-    public static Task BulkSyncAsync<T>(this DbContext dbContext, IEnumerable<T> entities, string? deleteWhere = null, int? timeoutOverride = 600) where T : class
+    public static Task BulkSyncAsync<T>(this DbContext dbContext, IEnumerable<T> entities, string? deleteWhere = null, int? timeoutOverride = 600, bool useContextConnection = true) where T : class
     {
-        var @operator = new BulkEfOperator(dbContext, timeoutOverride);
+        var @operator = new BulkEfOperator(dbContext, timeoutOverride, useContextConnection);
         return @operator.SyncAsync(entities, deleteWhere);
     }
 
-    public static Task BulkMergeAsync<T>(this DbContext dbContext, IEnumerable<T> entities, int? timeoutOverride = 600) where T : class
+    public static Task BulkMergeAsync<T>(this DbContext dbContext, IEnumerable<T> entities, int? timeoutOverride = 600, bool useContextConnection = true) where T : class
     {
-        var @operator = new BulkEfOperator(dbContext, timeoutOverride);
+        var @operator = new BulkEfOperator(dbContext, timeoutOverride, useContextConnection);
         return @operator.MergeAsync(entities);
     }
 
-    public static Task BulkInsertAsync<T>(this DbContext dbContext, IEnumerable<T> entities, int? timeoutOverride = 600) where T : class
+    public static Task BulkInsertAsync<T>(this DbContext dbContext, IEnumerable<T> entities, int? timeoutOverride = 600, bool useContextConnection = true) where T : class
     {
-        var @operator = new BulkEfOperator(dbContext, timeoutOverride);
+        var @operator = new BulkEfOperator(dbContext, timeoutOverride, useContextConnection);
         return @operator.InsertAsync(entities);
     }
 
